1ec8674cfd0f3a320b11f6d0ab8a154f01a1b63c
happinesslijian <47111417+happinesslijian@users.noreply.github.com>
2020-09-17 18:35:08 +0800

    Update messages.sh

--- 博客文章/PLG/messages.sh
+++ 博客文章/PLG/messages.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 echo -e '\033[1;31m 本脚本收集部署主机的/var/log/messages日志信息 \033[0m'
-echo -n "请输入被索引主机的主机名:"
-read hostname
+echo -n $HOSTNAME
 
 echo -e '\033[1;31m 写入配置文件到/etc/promtail/promtail-local-config.yaml \033[0m'
 cat >> /etc/promtail/promtail-local-config.yaml << EOF
-- job_name: 10.20.80.207
+- job_name: ${HOSTNAME}
   static_configs:
   - targets:
       - localhost
     labels:
-      job: ${hostname}-messages-logs
+      job: ${HOSTNAME}-messages-logs
       __path__: /var/log/messages
 EOF
 systemctl restart promtail 
